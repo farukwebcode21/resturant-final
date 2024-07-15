70483b03f81e42a81cad0a8fad23d9033a95fa44
farukwebcode21 <farukahmadmk@yahoo.com>
2024-07-15 22:34:08 +0600

    our menu ok

--- client_code/src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ client_code/src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,6 +1,7 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 import { useMenu } from "../../../hooks/useMenu";
+import Button from "../../Shared/Button/Button";
 
 const PopularMenu = () => {
   const [menu] = useMenu();
@@ -8,13 +9,14 @@ const PopularMenu = () => {
 
   return (
     <>
-      <section className="mb-11">
+      <section className="mb-4">
         <SectionTitle subHeading={"Check it Out"} heading={"From our menu"} />
-        <div className="grid md:grid-cols-2 gap-10">
+        <div className="grid md:grid-cols-2 gap-10 mb-10">
           {popular.map((item) => (
             <MenuItem key={item._id} item={item} />
           ))}
         </div>
+        <Button name={"view full menu"} />
       </section>
     </>
   );
